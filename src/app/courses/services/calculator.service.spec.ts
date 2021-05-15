ce3605699854c122ed7bdd4f1f3a6ca20a6049ed
Emanuel Ledesma <34849198+mannymaen@users.noreply.github.com>
2021-05-15 00:38:29 -0500

    lecture completed.

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,3 +1,4 @@
+import { TestBed } from "@angular/core/testing";
 import { CalculatorService } from "./calculator.service";
 import { LoggerService } from "./logger.service";
 
@@ -8,7 +9,15 @@ describe('CalulatorService', () => {
 
     beforeEach(() => {
         loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-        calculator = new CalculatorService(loggerSpy);
+
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                {provide: LoggerService, useValue: loggerSpy}
+            ]
+        })
+
+        calculator = TestBed.inject(CalculatorService);
     });
 
     it('should add two numbers', () => {
