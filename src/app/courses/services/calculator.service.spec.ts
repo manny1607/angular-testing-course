e4dc236420118785007d0d27ca44af198d7019db
Emanuel Ledesma <34849198+mannymaen@users.noreply.github.com>
2021-05-15 00:17:49 -0500

    lecture completed.

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -3,21 +3,25 @@ import { LoggerService } from "./logger.service";
 
 describe('CalulatorService', () => {
 
+    let calculator: CalculatorService,
+        loggerSpy: LoggerService;
+
+    beforeEach(() => {
+        loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+        calculator = new CalculatorService(loggerSpy);
+    });
 
     it('should add two numbers', () => {
 
         // const logger = new LoggerService();
         // spyOn(logger, 'log');
-        const logger = jasmine.createSpyObj('LoggerServicew', ['log']);
-        const calculator = new CalculatorService(logger);
         const result = calculator.add(2, 2);
 
         expect(result).toBe(4);
-        expect(logger.log).toHaveBeenCalledTimes(1);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
 
     it('should subtract two numbers', () => {
-        const calculator = new CalculatorService(new LoggerService());
         const result = calculator.subtract(2, 2);
 
         expect(result).toBe(0, 'unexpected subtraction result');
